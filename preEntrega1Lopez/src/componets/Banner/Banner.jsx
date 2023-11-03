c811d54078588b2e7eee028484ccec0fe55b1bd4
pipef104 <pipef104@gmail.com>
2023-11-02 21:35:03 -0500

    first commit

---
+++ preEntrega1Lopez/src/componets/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+  return (
+   <div className="banner">
+    <div className="banner-container">
+        <h1>Oasis Shop</h1>
+        <p>The best place to find suplements</p>
+    </div>
+   </div>
+  )
+}
+
+export default Banner
