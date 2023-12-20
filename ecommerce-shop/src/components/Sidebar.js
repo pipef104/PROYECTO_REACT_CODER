a32435bf9bbeeec9d77e4e38b52518697949aad2
pipef104 <pipef104@gmail.com>
2023-12-20 15:19:03 -0500

    Se carga ultimo cambio

--- ecommerce-shop/src/components/Sidebar.js
+++ ecommerce-shop/src/components/Sidebar.js
@@ -14,7 +14,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart, total } = useContext(CartContext);
+  const { cart, clearCart, total, itemAmount } = useContext(CartContext);
   return (
     <div
       className={`${
@@ -23,7 +23,9 @@ const Sidebar = () => {
   transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="Flex items-center justify-between py-6 border-b">
-        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        <div className="uppercase text-sm font-semibold">
+          Shopping Bag ({itemAmount})
+        </div>
         {/** icon */}
         <div
           onClick={handleClose}
@@ -32,19 +34,22 @@ const Sidebar = () => {
           <IoMdArrowForward className="text-2x1" />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b" >
+      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
       </div>
-      <div className="bg-pink-200 flex flex-col gap-y-3 py-4 mt-4 ">
+      <div className="flex flex-col gap-y-3 py-4 mt-4 ">
         <div className="flex w-full justify-between items-center">
           {/**Total */}
           <div className="uppercase font-semibold">
             <span className="mr-2">Total:</span>$ {parseFloat(total).toFixed(2)}
           </div>
           {/**clear cart icon */}
-          <div onClick={clearCart} className="cursor-pointer py-4 bg-pink-500 text-white w-12 h-12 flex justify-center items-center text-xl">
+          <div
+            onClick={clearCart}
+            className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl"
+          >
             <FiTrash2 />
           </div>
         </div>
